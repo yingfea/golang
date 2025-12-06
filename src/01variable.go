3c36ef267fa1cbe9bd7f6e8a446bf4df6e8da110
maolaiQaQ <maolaiqaq@outlook.com>
2025-12-06 14:54:04 +0800

    refactor: variable add global variable and local variable

--- src/01variable.go
+++ src/01variable.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
-func main(){
-	var num=1; 
-	var str1 = "hello world!";
-	fmt.Println(num);
-	fmt.Println(str1);
-	fmt.Printf("he say: %v",str1);
+// global variable
+var (
+	User string
+	Age int
+	Height float32
+)
 
+func main(){
+	var num=1
+	var str1 = "hello world!"
+	fmt.Println(num)
+	fmt.Println(str1)
+	fmt.Printf("he say: %v",str1)
+
+	// local variable
 	a := 10
 	b := 20
 	c := "hello! I am alex!"
@@ -20,5 +28,14 @@ func main(){
 		%f float
 		...
 	*/
-	fmt.Printf("a=%v,b=%v, and alex say:%v",a,b,c)
+	fmt.Printf("a=%v,b=%v, and alex say:%v \n",a,b,c)
+
+	var name string = "alex"
+	var age int = 20
+	var height float32 = 1.61
+	fmt.Printf("name=%v, age=%v, and how tall:%v \n",name, age,  height)
+	
+	User = "Hello"
+	fmt.Println(User)
+
 }
